1f44e507a64ffb0c4c79133aa4b39f2212e9419a
Drunken Kiwi <drunk3nkiwi@gmail.com>
2014-11-12 18:24:57 +0100

    Controller projet

---
+++ vendor/Dk/Controller/Project.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Controller des projets personnels
+ *
+ * @author GIL Morgan gilmorgan.pro@gmail.com
+ */
+namespace Dk\Controller;
+
+use Silex\Application;
+
+class Project
+{
+   public function main(Application $app)
+   {
+      return "";
+   }
+}
