697b85df2257328e1c166683e73cdbb8af17d541
Drunken Kiwi <drunk3nkiwi@gmail.com>
2014-11-12 19:57:56 +0100

    Controller contact

---
+++ vendor/Dk/Controller/Contact.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Controller de l'a propos
+ *
+ * @author GIL Morgan gilmorgan.pro@gmail.com
+ */
+namespace Dk\Controller;
+
+use Silex\Application;
+
+class Contact
+{
+   public function main(Application $app)
+   {
+      return "";
+   }
+}
